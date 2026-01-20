e4e193356bbf2e025ebf0106c1da00fdf05fa4e4
Qian <lsq1076603184@163.com>
2026-01-20 14:20:41 +0800

    feat: feat commit

---
+++ src/test/java/org/example/jdemo/JdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
